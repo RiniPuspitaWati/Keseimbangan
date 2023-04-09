8a1bf758e225709dfa3f589ee6d28c4c6b5902db
RiniPuspitaWati <112569358+RiniPuspitaWati@users.noreply.github.com>
2023-04-09 14:14:42 +0700

    Add files via upload

---
+++ Keseimbangan.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int a,i,u,e;
+	cin>>a;
+	cin>>i;
+	cin>>u;
+	cin>>e;
+	
+	if(a*i==u-e){
+		cout<<"True\n";
+	}
+	else{
+		cout<<"False\n";
+	}
+	
+	return 0;
+	
+}
